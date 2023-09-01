f292ed985cab3f055b618173b8535ed40f3e00f2
Luis F Cruz Ramos <luisfernandocruzramos@gmail.com>
2023-09-01 20:37:10 +0200

    Navigation update to add new fragments (#80)

---
+++ app/src/main/java/com/tfm/musiccommunityapp/ui/search/SearchFragment.kt
@@ -0,0 +1,7 @@
+package com.tfm.musiccommunityapp.ui.search
+
+import com.tfm.musiccommunityapp.R
+import com.tfm.musiccommunityapp.base.BaseFragment
+
+class SearchFragment: BaseFragment(R.layout.search_fragment) {
+}
